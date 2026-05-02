d80f1a0b85273c99ba0328862a535e54125977d9
kamau-David <dayvieek@gmail.com>
2026-05-02 13:00:00 +0300

    feat(03): add script to run join queries

---
+++ 03-joins/run.js
@@ -0,0 +1,17 @@
+const Database = require("better-sqlite3");
+const fs = require("fs");
+const path = require("path");
+
+const db = new Database(":memory:");
+db.exec(fs.readFileSync(path.join(__dirname, "schema.sql"), "utf-8"));
+
+const queries = fs
+  .readFileSync(path.join(__dirname, "queries.sql"), "utf-8")
+  .split(";")
+  .map((s) => s.trim())
+  .filter((s) => s && s.toUpperCase().includes("SELECT"));
+
+queries.forEach((q, i) => {
+  console.log(`\n--- Query ${i + 1} ---`);
+  console.table(db.prepare(q).all());
+});
